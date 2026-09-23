152051f62e9025948e71b779800c429688990417
Tigon32 <Anton@junlen.se>
2026-09-23 11:48:43 +0200

    Add webpack compile smoke build

---
+++ webpack.config.cjs
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'archimate-js.js',
+    library: {
+      type: 'module'
+    }
+  },
+  experiments: {
+    outputModule: true
+  }
+};
